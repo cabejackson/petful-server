fa9f6341b920bed892fe962919fb67756ddc43bd
Caleb Jackson <jacksoncaleb.calebjackson@gmail.com>
2020-11-19 19:42:46 -0500

    fixed queue issue

--- modules/queue/Queue.js
+++ modules/queue/Queue.js
@@ -1,98 +1,81 @@
 class Node {
-  constructor(data) {
-    this.value = data;
+  constructor(value) {
+    this.value = value;
     this.next = null;
   }
 }
 
-
-
 class Queue {
   constructor() {
     // Set initial data.
-    this.storage = {};
-    this.head = null;
-    this.tail = null;
-
+    //this.storage = {};
+    this.first = null;
+    this.last = null;
   }
-
   // adding to the back
   enqueue(data) {
     // Add some data to the queue.
-    // "if there is no first, enqueue adds a new first, otherwise it just adds to the back of the line(aka the tail)"
-    //"this.storage AT this.tail = the data"
     const node = new Node(data);
-
-    if (!this.head) {
-      this.head = node;
+    // "if there is no first, enqueue adds a new first, otherwise it just adds to the back of the line
+    if (!this.first) {
+      this.first = node;
     }
 
-    if (this.tail) {
-      this.tail.next = node;
+    if (this.last) {
+      this.last.next = node;
     }
     //this becomes the last item in the queue
-    this.tail = node;
 
-    this.storage[this.tail] = data;
-    // // console.log(data);
-    //counter 
-    this.tail++;
+    this.last = node;
   }
 
   //removing from the front
+
   dequeue() {
     // Remove some data from the queue.
-
-    if (!this.head) {
+    if (!this.first) {
       return;
     }
-    const node = this.head;
-    this.head = this.head.next;
+    const node = this.first;
+    this.first = this.first.next;
 
-    if (node === this.tail) {
-      this.tail = null;
+    if (node === this.last) {
+      this.last = null;
     }
     return node.value;
-    // let removed = this.storage[this.head];
-    // delete this.storage[this.head];
-    // this.head++;
-    // return removed;
   }
 
   show() {
     // Return the next item in the queue.
-    if (!this.head) {
+    //const node = this.first.value;
+    const node = this.first;
+    //const next = node.next;
+    if (!this.first) {
       return 'nothing is available';
     }
-    //const node = this.head.value;
-    const node = this.head;
-    // const next = node.next
     if (node === null) {
       return;
     }
 
     return node;
 
-    // if (!this.head) {
-    //   return 'nothing is available';
-    // }
-    // const firstPet = this.head.value;
-    // // console.log('here', firstPet)
-    // return firstPet;
   }
 
   all() {
     // Return all items in the queue.
-
-    let node = this.head;
-    let all = [];
+    let node = this.first;
+    let array = [];
     console.log('outside', node);
-    while (node !== null) {
-      all.push(node.value);
+
+    while (node) {
+      array.push(node.value);
       node = node.next;
       console.log('inside', node);
+
     }
-    return all;
+
+    return array;
+
   }
 }
 
